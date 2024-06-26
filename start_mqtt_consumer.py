1025459cef09394fbafda9935ef424a1b6a72177
Chris LaGreca <clagreca03@gmail.com>
2024-06-25 20:33:39 -0400

    added on_subscribe and on_log callbacks

--- start_mqtt_consumer.py
+++ start_mqtt_consumer.py
@@ -6,16 +6,33 @@ import yaml
 
 
 def on_connect(client, userdata, flags, reason_code, properties=None):
+    """ Callback function for the on_connect event """
 
-    print(f"Connected with reason code: {reason_code}")
-    client.subscribe(config.get('mqtt.topic'))
+    if reason_code == 0:
+        print(f"Connected with reason code: {reason_code}")
+        client.subscribe(config.get('mqtt.topic'))
+    else:
+        print(f"Connection failed with reason code: {reason_code}")
+
+
+def on_subscribe(client, userdata, mid, reason_code, properties=None):
+    """ Callback function for the on_subscribe event """
+
+    print(f"Successfully subscribed to topics...")
+
     
-
 def on_message(client, userdata, message):
+    """ Callback function for the on_message event """
     
     print('\n')
     payload = json.loads(message.payload.decode("utf-8"))
     print(f"Topic: {message.topic}\nPayload: {payload}")
+
+
+def on_log(client, userdata, level, buf):
+    """ Callback function for the on_log event """
+
+    print(f"Level: {level}, Message: {buf}")
 
 
 def get_config():
@@ -49,15 +66,20 @@ if __name__ == '__main__':
     env = parse_arguments()
     config = get_config().get('mqtt-config').get(env)
 
-
+    # create a client instance
     client = paho.Client(paho.CallbackAPIVersion.VERSION2, client_id=config.get('mqtt.client.id'))
     if config.get('mqtt.require.login'): client.username_pw_set(username=config.get('mqtt.user'), password=config.get('mqtt.pass'))
+    if config.get('mqtt.tls'): client.tls_set()
     
+
     # register callbacks
     client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
     client.on_message = on_message
 
-    if config.get('mqtt.tls'): client.tls_set()
+    # connect optional parameters
+    # client.on_log = on_log
+
     print(f"Connecting to the MQTT broker {config.get('mqtt.server')} on port {config.get('mqtt.port')}")
     client.connect(config.get('mqtt.server'), config.get('mqtt.port'))
     client.loop_start()
